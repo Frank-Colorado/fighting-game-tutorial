27b68146d888e4072156f846a2acc472e2e384b2
Frank-Colorado <chawnkiee@gmail.com>
2023-04-15 21:54:46 -0400

    created a class called 'Player'

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,20 @@ const p1HealSound = document.getElementById("p1heal");
 const p2AttackSound = document.getElementById("p2attack");
 const p2HealSound = document.getElementById("p2heal");
 const victorySound = document.getElementById("victory");
+
+// This is a class called 'Player'
+class Player {
+  constructor(health = 100, attackDmg) {
+    this.health = health;
+    this.attackDmg = attackDmg;
+  }
+
+  // This is a method that will cause the player to heal
+  heal(target) {
+    // heals target for x amount
+  }
+  // This is a method that will cause the player to hit the target
+  strike(target, user, dmgAmount) {
+    // user strikes target for x amount
+  }
+}

a0caed0b04aae4c83d35d9defb4e659016fe69d8
Frank-Colorado <chawnkiee@gmail.com>
2023-04-15 21:46:06 -0400

    added variables for audio files in JS file

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// Sound Variables
+const p1AttackSound = document.getElementById("p1attack");
+const p1HealSound = document.getElementById("p1heal");
+const p2AttackSound = document.getElementById("p2attack");
+const p2HealSound = document.getElementById("p2heal");
+const victorySound = document.getElementById("victory");
